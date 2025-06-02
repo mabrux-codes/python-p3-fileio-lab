1d836c49436b987ad84a2ebc394cf59c72f4f298
Abel Lukwaro <abelmabruke@gmail.com>
2025-06-02 17:48:11 +0300

    Initial commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    # file_name can be a Path or str without .txt extension
+    path = str(file_name) + '.txt'
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    path = str(file_name) + '.txt'
+    with open(path, 'a', encoding='utf-8') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    path = str(file_name) + '.txt'
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
